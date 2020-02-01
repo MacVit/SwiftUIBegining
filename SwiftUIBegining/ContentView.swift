3cf9a5227957bf80a1b53f2b25c323c7abefb01f
Vitalii <vitalikmaksumlyk@gmail.com>
2020-02-01 14:35:48 +0200

    Initial Commit

---
+++ SwiftUIBegining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBegining
+//
+//  Created by Vitalii Maksymliuk on 01.02.2020.
+//  Copyright © 2020 Vitalii Maksymliuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
